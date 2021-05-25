8b4fc76aa9e1832e1dcd3a97fe1880a83e90c0a1
huxt27670 <huxt27670@hundsun.com>
2021-05-25 15:39:27 +0800

    修改脚本文件

--- Modify_suffix/Modify_suffix.py
+++ Modify_suffix/Modify_suffix.py
@@ -5,25 +5,35 @@ import sys
 批量修改文件夹下所有文件的后缀名
 '''
 
-old_suffix = sys.argv[1]
-new_suffix = sys.argv[2]
-filepath = sys.argv[3]
-count = 0
+def Modify_suffix():
+    old_suffix = sys.argv[1]
+    new_suffix = sys.argv[2]
+    filepath = sys.argv[3]
+    count = 0
 
-if filepath == ' ' or filepath == None:
-    filepath = os.path.dirname(os.path.abspath(__file__))
+    if filepath == ' ' or filepath == None:
+        filepath = os.path.dirname(os.path.abspath(__file__))
 
-files = os.listdir(filepath)
+    files = os.listdir(filepath)
 
-for file in files:
-    portion = os.path.splitext(file)
-    if portion[1] == old_suffix:
-        count = count + 1
-        newname = portion[0] + new_suffix
-        os.chdir(filepath)
-        os.rename(file, newname)
-if count == 0:
-    print("\n该文件夹下没有后缀为" + old_suffix + "的文件")
-else:
-    print("\n后缀已经修改为" + new_suffix)
+    for file in files:
+        portion = file.split('.')
+        portionLength = len(portion)
+        if portion[portionLength-1] == old_suffix:
+            count = count + 1
+            filename = []
+            for i in range(0,portionLength-1):
+                filename.append(portion[i])
+                filename = ".".join(filename)
+            newname = filename + "." + new_suffix
+            os.chdir(filepath)
+            os.rename(file, newname)
+    if count == 0:
+        print("\n该文件夹下没有后缀为" + old_suffix + "的文件")
+    else:
+        print("\n后缀已经修改为" + new_suffix)
+
+
+if __name__ == '__main__':
+    Modify_suffix()
 
